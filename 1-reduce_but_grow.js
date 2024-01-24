d6bcffe394b95503d57c3e0d2cdc1664befbd739
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2024-01-25 01:26:31 +0700

    day 1/366

---
+++ 1-reduce_but_grow.js
@@ -0,0 +1,18 @@
+// CARA 1
+function grow(x){
+    let result = x[0];
+    for(let i = 1 ; i < x.length; i++)
+    {
+        /* result = x[i] * result; */
+        result *= x[i];
+    }
+    return result;
+}
+
+// CARA 2
+function grow(x){
+    const result = x.reduce((acc,curr) => acc * curr, 1);
+    return result;
+}
+
+console.log(grow([1,2,3,4]))
