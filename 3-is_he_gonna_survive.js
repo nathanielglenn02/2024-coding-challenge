509b9988aab761653aa33562ad38d2044d3f3516
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2024-01-25 23:52:14 +0700

    TEST CODE DAY-3

--- 3-is_he_gonna_survive.js
+++ 3-is_he_gonna_survive.js
@@ -1,6 +1,20 @@
+//DAY 3/366
+
 function hero(bullets, dragons)
 {
-    
+    if(bullets/2 >= dragons)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
+
+// function hero(bullets, dragons)
+// {
+//     return (bullets / 2 >= dragons) ? true : false;
+// }
 
 console.log(hero(10,5))
