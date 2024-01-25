be156db840aeab22208aca76bc0866c0ca15b8cf
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2024-01-25 23:36:52 +0700

    TEST CODE DAY 2/366

---
+++ 2-reversed_sequence.js
@@ -0,0 +1,13 @@
+// const reverseSeq = (n) => {
+//     const result = [];
+//     for(let i = n; i >= 1; i--)
+//     {
+//         result.push(i);
+//     }
+//     return result;
+//   };
+
+
+const reverseSeq = length => Array.from({length}, () => length--);
+
+console.log(reverseSeq(6));
