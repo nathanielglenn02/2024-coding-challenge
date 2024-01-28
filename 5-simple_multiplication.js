0af0706028901f67c38f8efaf9bd180ee381e624
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2024-01-28 22:20:39 +0700

    TEST CODE DAY-5

---
+++ 5-simple_multiplication.js
@@ -0,0 +1,14 @@
+//DAY 5/366
+
+function simpleMultiplication(number) {
+    if(number % 2 == 0)
+      {
+        return number * 8;
+      }
+  else
+    {
+      return number * 9;
+    }
+}
+
+console.log(simpleMultiplication(7))
