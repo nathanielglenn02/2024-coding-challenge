a4ce8e019277f55444d234b844692681ede7239b
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2024-01-28 23:00:48 +0700

    TEST CODE DAY-5

---
+++ 5-sum_arrays.js
@@ -0,0 +1,19 @@
+//DAY 5/366
+
+function sum (numbers) {
+    if(numbers.length == [])
+    {
+        return 0;
+    }
+    else
+    {
+        result = numbers[0];
+        for (let i = 1; i < numbers.length; i++)
+        {
+            result += numbers[i]
+        }
+        return result;
+    }
+};
+
+console.log(sum([]))
