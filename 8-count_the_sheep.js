79c5fcc35d048c651b753dcfb3b6dbd93452af29
nathanielglenn02 <106645313+nathanielglenn02@users.noreply.github.com>
2024-02-02 23:12:28 +0700

    TEST CODE DAY-8 (8 KYU)

---
+++ 8-count_the_sheep.js
@@ -0,0 +1,14 @@
+// DAY 8/366
+
+var countSheep = function (num){
+    let result = "";
+    for(let i = 1; i <= num; i++){
+        result += i + " sheep..."
+    }
+    return result
+  }
+
+
+
+
+  console.log(countSheep(3))
---
+++ 9-find_max_min.js
@@ -0,0 +1,16 @@
+// DAY 9/366
+
+
+var min = function(list){
+    
+    return list[0];
+}
+
+var max = function(list){
+    
+    return list[0];
+}
+
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110]))
+console.log(max([4,6,2,1,9,63,-134,566]))
